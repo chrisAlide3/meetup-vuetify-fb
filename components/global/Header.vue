abccc8f79131d16ca2180edc579c1407e096ee70
krispea3 <christian.bracher@gmail.com>
2019-07-16 13:31:49 +0800

    Added dynamic btn generation on toolbar

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -4,6 +4,7 @@
     <v-navigation-drawer
       v-model="drawer"
       app
+      temporary
     >
       <v-toolbar flat dark class="primary">
         <v-list>
@@ -21,7 +22,8 @@
         <v-list-tile
           v-for="item in items"
           :key="item.title"
-          @click="$router.push(item.route)"
+          router
+          :to="item.route"
         >
           <v-list-tile-action router to="item.route">
             <v-icon>
@@ -45,59 +47,25 @@
       />
 
       <v-toolbar-title class="white--text">
-        <v-btn flat @click="$router.push('/')">
-          Meetups
-        </v-btn>
+        <nuxt-link to="/" tag="span" style="cursor: pointer">Meetups</nuxt-link>
       </v-toolbar-title>
 
       <v-spacer />
 
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat @click="$router.push('/meetups')">
+        <v-btn
+          flat 
+          v-for="item in items" :key="item.title"
+          router
+          :to="item.route"
+        >
           <v-icon left>
-            list
-          </v-icon>
-          Meetups
-        </v-btn>
-        <v-btn flat @click="$router.push('/auth?isLogin=false')">
-          <v-icon left>
-            account_circle
-          </v-icon>
-          Register
-        </v-btn>
-        <v-btn flat @click="$router.push('/auth?isLogin=true')">
-          <v-icon left>
-            exit_to_app
-          </v-icon>
-          Login
-        </v-btn>
-
-      </v-toolbar-items>
-      <!-- <v-list-tile
-        v-for="item in items"
-        :key="item.title"
-        class="hidden-sm-and-down"
-        @click="$router.push(item.route)"
-      > -->
-        <!-- <v-list-tile-action>
-          <v-icon>
             {{ item.icon }}
           </v-icon>
-        </v-list-tile-action> -->
+        {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
 
-        <!-- <v-list-tile-content>
-          <v-layout row>
-            <v-flex>
-              <v-icon>
-                {{ item.icon }}
-              </v-icon>
-              <v-list-tile-title>
-                {{ item.title }}
-              </v-list-tile-title>
-            </v-flex>
-          </v-layout>
-        </v-list-tile-content>
-      </v-list-tile> -->
     </v-toolbar>
 
   </nav>
@@ -108,8 +76,8 @@ export default {
   data() {
     return {
       items: [
-        { title: 'Home', icon: 'home', route: '/' },
-        { title: 'Meetups', icon: 'list', route: '/meetups' },
+        { title: 'View Meetups', icon: 'supervisor_account', route: '/meetups' },
+        { title: 'Profile', icon: 'person', route: '/profile' },
         { title: 'Login', icon: 'exit_to_app', route: '/auth?isLogin=true' },
         { title: 'Register', icon: 'account_circle', route: '/auth?isLogin=false' }
       ],
