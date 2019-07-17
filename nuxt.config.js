44fe4eab378adf338b1ba2f85fab282aaa1d363d
krispea3 <christian.bracher@gmail.com>
2019-07-17 09:31:14 +0800

    Installed firebase sdk and initialized with plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~plugins/firebase.js'
   ],
   /*
   ** Nuxt.js modules
---
+++ plugins/firebase.js
@@ -0,0 +1,10 @@
+// import Vue from 'vue'
+import * as firebase from 'firebase'
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyAgB7MrRcKXJUr5jSpDRCKzi3ICqcg3wgk',
+  authDomain: 'meetup-vuetify-fb.firebaseapp.com',
+  databaseURL: 'https://meetup-vuetify-fb.firebaseio.com',
+  projectId: 'meetup-vuetify-fb',
+  storageBucket: 'meetup-vuetify-fb.appspot.com'
+})
