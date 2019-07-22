ba32064c89ca3a4dca25fac39fa7f794e42d7b79
krispea3 <christian.bracher@gmail.com>
2019-07-22 13:46:48 +0800

    Added basic routing for meetups

--- pages/admin/meetups/_id/index.vue
+++ pages/admin/meetups/_id/index.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>Edit meetup route</h1>
+  <div>
+    <h1>Edit meetup route</h1>
+    <hr>
+    <Form :meetup="meetup"/>
+  </div>
 </template>
+
+<script>
+import Form from '~/components/meetups/Form'
+
+export default {
+  components: {
+    Form
+  },
+  computed: {
+    meetup () {
+
+    }
+  }  
+}
+</script>
--- pages/admin/meetups/new/index.vue
+++ pages/admin/meetups/new/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>New meetup route</h1>
+  <div>
+    <h1>Add meetup route</h1>
+    <hr>
+    <Form />
+</div>
 </template>
+
+<script>
+import Form from '~/components/meetups/Form'
+
+export default {
+  components: {
+    Form
+  },
+}
+</script>
